8c2309114631ea2c87f37d41741fef59e5fb3c11
Ashton Honnecke <ashton@pixelstub.com>
2022-03-30 12:59:18 -0600

    RC one
    Signed-off-by: Ashton Honnecke <ashton@pixelstub.com>

--- README.rst
+++ README.rst
@@ -48,8 +48,3 @@ Note
 This project has been set up using PyScaffold 4.2.1. For details and usage
 information on PyScaffold see https://pyscaffold.org/.
 
-=======
-Install editable for development
-=======
-
-sudo pip install -e .
--- src/dolores/skeleton.py
+++ src/dolores/skeleton.py
@@ -21,10 +21,20 @@ References:
 """
 
 import argparse
+import json
 import logging
+import secrets
+import signal
 import sys
+from subprocess import PIPE, Popen
+from time import sleep
 
-from dolores import __version__
+import boto3
+from botocore.exceptions import ClientError
+from prompt_toolkit import prompt
+from prompt_toolkit.completion import WordCompleter
+
+from dolores import __version__, main
 
 __author__ = "Ashton Honnecke"
 __copyright__ = "Ashton Honnecke"
@@ -33,117 +43,232 @@ __license__ = "MIT"
 _logger = logging.getLogger(__name__)
 
 
-# ---- Python API ----
-# The functions defined in this section can be imported by users in their
-# Python scripts/interactive interpreter, e.g. via
-# `from dolores.skeleton import fib`,
-# when using this Python module as a library.
+class SNSTopicListener:
+    def __init__(self, account_id, topic, debug=False, sig=signal.SIGINT):
+        self.sig = sig
+        self.topic = topic
+        self.account_id = account_id
+        self.debug = debug
+        self.create_queue()
+        self.subscribe()
+
+    def create_queue(self):
+        self.sns_client = boto3.client("sns")
+        response = self.sns_client.list_topics()
+        arns = [x["TopicArn"] for x in response["Topics"]]
+
+        if not self.topic:
+            topic_completer = WordCompleter([x.split(":")[-1] for x in arns])
+            self.topic = prompt("Choose topic: ", completer=topic_completer)
+
+        matches = [arn for arn in arns if self.topic in arn]
+
+        self.topic_arn = matches[0]
+
+        rando_hex = secrets.token_hex(5)
+        sqs_queue_name = f"sns-listener_{self.topic}_{rando_hex}"
+        email = f"{sqs_queue_name}@honnecke.us"
+
+        # Get the service resource
+        sqs_client = boto3.resource("sqs")
+
+        # Create the queue. This returns an SQS.Queue instance
+        self.sqs_queue = sqs_client.create_queue(
+            QueueName=sqs_queue_name, Attributes={"DelaySeconds": "5"}
+        )
+
+        self.sqs_queue_arn = self.sqs_queue.attributes["QueueArn"]
+        if ":sqs." in self.sqs_queue_arn:
+            self.sqs_queue_arn = self.sqs_queue_arn.replace(":sqs.", ":")
+
+        formattend_policy = json.dumps(
+            {
+                "Version": "2008-10-17",
+                "Id": f"__auto_policy_{rando_hex}",
+                "Statement": [
+                    {
+                        "Sid": "__owner_statement",
+                        "Effect": "Allow",
+                        "Principal": {"AWS": f"arn:aws:iam::{self.account_id}:root"},
+                        "Action": "SQS:*",
+                        "Resource": self.sqs_queue_arn,
+                    },
+                    {
+                        "Sid": "topic-subscription-arn:" + self.topic_arn,
+                        "Effect": "Allow",
+                        "Principal": {"AWS": "*"},
+                        "Action": "SQS:SendMessage",
+                        "Resource": self.sqs_queue_arn,
+                        "Condition": {"ArnLike": {"aws:SourceArn": self.topic_arn}},
+                    },
+                ],
+            }
+        )
+
+        self.sqs_queue.set_attributes(Attributes={"Policy": formattend_policy})
+
+    def subscribe(self):
+        # Subscribe SQS queue to SNS
+        return self.sns_client.subscribe(
+            TopicArn=self.topic_arn, Protocol="sqs", Endpoint=self.sqs_queue_arn
+        )
+
+    def __enter__(self):
+
+        self.interrupted = False
+        self.released = False
+
+        self.original_handler = signal.getsignal(self.sig)
+
+        def handler(signum, frame):
+            self.release()
+            self.interrupted = True
+
+        signal.signal(self.sig, handler)
+
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.remove_queue()
+        self.release()
+
+    def release(self):
+
+        if self.released:
+            return False
+
+        signal.signal(self.sig, self.original_handler)
+
+        self.released = True
+
+        return True
+
+    def remove_queue(self):
+        try:
+            self.sqs_queue.delete()
+            print(f"Deleted queue with URL {self.sqs_queue.url}.")
+            _logger.info("Deleted queue with URL=%s.", self.sqs_queue.url)
+        except ClientError as error:
+            _logger.exception("Couldn't delete queue with URL=%s!", self.sqs_queue.url)
+            raise error
+
+    def listen(self):
+        messages = self.sqs_queue.receive_messages(
+            AttributeNames=["All"],
+            MessageAttributeNames=["All"],
+            VisibilityTimeout=15,
+            WaitTimeSeconds=1,
+            MaxNumberOfMessages=1,
+        )
+        if messages:
+            m = messages[0]  # pylint: disable=invalid-name
+
+            if self.debug:
+                print("Dropping into debugger for inspection")
+                print("Local message variable is 'm'")
+                print("PDB commands: 'c' to continue, 'exit()' to exit")
+                breakpoint()
+            else:
+                print(f"Recieved message, {m.body}")
+
+            m.delete()
+
+        print("Listening...")
+        sleep(1)
 
 
-def fib(n):
-    """Fibonacci example function
-
-    Args:
-      n (int): integer
-
-    Returns:
-      int: n-th Fibonacci number
+def parse_args():
     """
-    assert n > 0
-    a, b = 1, 1
-    for _i in range(n - 1):
-        a, b = b, a + b
-    return a
-
-
-# ---- CLI ----
-# The functions defined in this section are wrappers around the main Python
-# API allowing them to be called directly from the terminal as a CLI
-# executable/script.
-
-
-def parse_args(args):
-    """Parse command line parameters
-
-    Args:
-      args (List[str]): command line parameters as list of strings
-          (for example  ``["--help"]``).
-
-    Returns:
-      :obj:`argparse.Namespace`: command line parameters namespace
+    Extract the CLI arguments from argparse
     """
-    parser = argparse.ArgumentParser(description="Just a Fibonacci demonstration")
-    parser.add_argument(
-        "--version",
-        action="version",
-        version="dolores {ver}".format(ver=__version__),
+    _parser = argparse.ArgumentParser(description="Listen to an SNS topic")
+
+    _parser.add_argument(
+        "-p",
+        "--profile",
+        help="AWS profile name",
     )
-    parser.add_argument(dest="n", help="n-th Fibonacci number", type=int, metavar="INT")
-    parser.add_argument(
-        "-v",
-        "--verbose",
-        dest="loglevel",
-        help="set loglevel to INFO",
-        action="store_const",
-        const=logging.INFO,
+    _parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Drop into debugger to inspect message",
     )
-    parser.add_argument(
-        "-vv",
-        "--very-verbose",
-        dest="loglevel",
-        help="set loglevel to DEBUG",
-        action="store_const",
-        const=logging.DEBUG,
+    _parser.add_argument(
+        "-t",
+        "--topic",
+        help="SNS topic name",
+        required=False,
     )
-    return parser.parse_args(args)
+    return _parser.parse_args()
 
 
-def setup_logging(loglevel):
-    """Setup basic logging
+def get_caller_identity():
+    if identity := aws_cli_execute(["sts", "get-caller-identity"]):
+        return json.loads(identity)
+    return None
 
-    Args:
-      loglevel (int): minimum loglevel for emitting messages
+
+def get_config_identity():
+    return aws_cli_execute(["configure", "list"]).split("\n")
+
+
+def get_available_profiles():
+    return aws_cli_execute(["configure", "list-profiles"]).split("\n")
+
+
+def is_logged_in() -> bool:
     """
-    logformat = "[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-    logging.basicConfig(
-        level=loglevel, stream=sys.stdout, format=logformat, datefmt="%Y-%m-%d %H:%M:%S"
+    Check to see if the user is logged in to AWS
+    """
+    return bool(get_caller_identity())
+
+
+def get_account_id() -> bool:
+    return get_caller_identity().get("Account")
+
+
+def get_profile():
+    profiles = get_available_profiles()
+    profile_completer = WordCompleter(profiles)
+    return prompt(
+        "Profile was not passed, choose a profile: ", completer=profile_completer
     )
 
 
-def main(args):
-    """Wrapper allowing :func:`fib` to be called with string arguments in a CLI fashion
+def cli_execute(command):
+    with Popen(command, stdin=PIPE, stdout=PIPE, stderr=PIPE) as process:
+        output, err = process.communicate(
+            b"input data that is passed to subprocess' stdin"
+        )
+        if err:
+            print(err.decode("utf-8"))
 
-    Instead of returning the value from :func:`fib`, it prints the result to the
-    ``stdout`` in a nicely formatted message.
-
-    Args:
-      args (List[str]): command line parameters as list of strings
-          (for example  ``["--verbose", "42"]``).
-    """
-    args = parse_args(args)
-    setup_logging(args.loglevel)
-    _logger.debug("Starting crazy calculations...")
-    print("The {}-th Fibonacci number is {}".format(args.n, fib(args.n)))
-    _logger.info("Script ends here")
+    return output.decode("utf-8")
 
 
-def run():
-    """Calls :func:`main` passing the CLI arguments extracted from :obj:`sys.argv`
+def aws_cli_execute(command):
+    command.insert(0, "aws")
+    return cli_execute(command)
 
-    This function can be used as entry point to create console scripts with setuptools.
-    """
-    main(sys.argv[1:])
+
+def main():
+    args = parse_args()
+
+    if not is_logged_in():
+        print("Please log in to AWS cli")
+        sys.exit(1)
+
+    if not args.profile:
+        args.profile = get_profile()
+
+    boto3.setup_default_session(profile_name=args.profile)
+    account_id = get_account_id()
+
+    with SNSTopicListener(account_id, args.topic, args.debug) as dolores:
+        while not dolores.released:
+            dolores.listen()
 
 
 if __name__ == "__main__":
-    # ^  This is a guard statement that will prevent the following code from
-    #    being executed in the case someone imports this file instead of
-    #    executing it as a script.
-    #    https://docs.python.org/3/library/__main__.html
-
-    # After installing your project with pip, users can also run your Python
-    # modules as scripts via the ``-m`` flag, as defined in PEP 338::
-    #
-    #     python -m dolores.skeleton 42
-    #
-    run()
+    main()
